9f84eb1da302ebbfacf733043e4e886d90c80473
GerasimenkoJulia <bussi93@mail.ru>
2014-08-13 23:04:08 +0400

    BD
    с учетом пожеланий

--- task/task.sql
+++ task/task.sql
@@ -3,58 +3,43 @@ create database Logiweb;
 use Logiweb;
 
 create table Trucks (
-    idTruck int unsigned auto_increment primary key,
+    id int unsigned auto_increment primary key,
     reg_number char(7),
     capacity int,
     drivers_qty int
 );
 
-create table Managers (
-    idManager int unsigned auto_increment primary key,
-    full_name char(30)
-);
-
 create table Orders (
-    idOrder int unsigned auto_increment primary key,
+    id int unsigned auto_increment primary key,
     truckId int unsigned,
     foreign key (truckId)
-        REFERENCES Trucks (idTruck),
-	managerId int unsigned,
-    foreign key (managerId)
-        REFERENCES Managers (idManager),
-    order_status char(11) NOT NULL default 'create'
-);
-
-create table Drivers (
-    idDriver int unsigned auto_increment primary key,
-    full_name char(30),
-    license int(11),
-    driver_status char(12) NOT NULL default 'not on shift',
-    orderId int unsigned,
-    foreign key (orderId)
-        REFERENCES Orders (idOrder)
-);
-
-
-create table Order_items (
-    idItem int unsigned auto_increment primary key,
-    orderId int unsigned,
-    foreign key (orderId)
-        REFERENCES Orders (idOrder),
+        REFERENCES Trucks (id),
+    order_state char(11) NOT NULL default 'create',
     gps decimal,
     loading varchar(50),
-    weight int,
-    delivery_status char(3) NOT NULL default 'No'
+    weight int
 );
 
+create table Users (
+    id int unsigned auto_increment primary key,
+    full_name char(30),
+    license int(11) default 0,
+    driver_state char(12) NOT NULL default 'not on shift',
+    orderId int unsigned default 0,
+    foreign key (orderId)
+        REFERENCES Orders (id),
+    pwd int(4),
+    isManager boolean default false
+);
 
 create table Orders_Statuses (
+    id int unsigned auto_increment primary key,
     orderId int unsigned,
     foreign key (orderId)
-        REFERENCES Orders (idOrder),
-    statys char(11) NOT NULL,
-    time_change_of_status timestamp,
-    UNIQUE KEY (orderId , statys)
+        REFERENCES Orders (id),
+    state char(11) NOT NULL,
+    time timestamp,
+    UNIQUE KEY (orderId , state)
 );
 	
 
